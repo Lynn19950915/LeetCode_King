39e787689e083cc2c9bb831b560c9583721657dc
Lynn Chen <samuel19950915@gmail.com>
2023-05-04 11:43:33 +0800

    first commit

---
+++ Easy (2)/596. Classes More Than 5 Students.php
@@ -0,0 +1,9 @@
+596. Classes More Than 5 Students
+
+問題：Courses 資料表以 (student, class) 為主鍵記錄了選課資訊，列出學生有超過四位的課程。
+
+-----
+解一：
+
+- SELECT class FROM Courses GROUP BY class HAVING COUNT(*)>=5;
+- SELECT class FROM Courses GROUP BY class HAVING COUNT(student)>=5;
---
+++ Tags/Array (1)/1431. Kids With the Greatest Number of Candies.js
@@ -0,0 +1,5 @@
+1431. Kids With the Greatest Number of Candies
+
+1. 找出陣列的最大值，並逐一比較各元素加上 extra 後是否不小於它。
+
+2. 承上，也可直接輸出於 candies 覆蓋原始值 (.forEach) 或利用 .map 回傳陣列，都能達到節省空間。
